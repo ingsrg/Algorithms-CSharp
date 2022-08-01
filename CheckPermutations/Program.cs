eb39b85f93185d7a6d3eec4278d1b799a2d3116a
Sergio Ramirez <ingsrg@gmail.com>
2022-08-01 17:23:59 -0500

    initial

---
+++ CheckPermutations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CheckPermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
