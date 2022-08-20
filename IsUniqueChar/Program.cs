b6af9cd539d570a8b3be28cd358c7064a25a0414
Sergio Ramirez <ingsrg@gmail.com>
2022-08-20 15:08:52 -0500

    IsUniqueChars

---
+++ IsUniqueChar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace IsUniqueChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsUniqueChar("Helo Ward"));
+        }
+
+        static bool IsUniqueChar(string w)
+        {
+            int checker = 0;
+
+            for(int i=0; i < w.Length; i++)
+            {
+                int val = w[i] - 'a';
+                if( (int) ( checker & 1 << val) > 0   )
+                    return false;
+
+                checker |= (1 << val);
+            }
+            return true;
+        }
+
+        static bool IsUniqueChar2(string w)
+        {
+            if(w.Length > 128)
+                return false;
+
+            bool[] array = new bool[128];
+
+            for(int i=0; i < w.Length; i++)
+            {
+                int val = w[i];
+                if(array[val])
+                    return false;
+
+                array[w[i]] = true;
+            }
+            return true;
+        }
+    }
+}
