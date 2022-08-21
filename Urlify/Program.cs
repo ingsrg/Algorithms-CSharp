f2dc0cfbf49ebeda8f6a75b80618523f2f194b7b
Sergio Ramirez <ingsrg@gmail.com>
2022-08-20 20:01:00 -0500

    Urlify

---
+++ Urlify/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Urlify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Urlify("He llo World!      ", 13));
+        }
+
+        static string Urlify(string str, int len)
+        {
+            int idx = len - 1;
+            int lastIdx = str.Length - 1;
+            char[] strArray = new char[str.Length];
+
+            for(int i=idx; i >= 0; i--)
+            {
+                if(str[i] == ' ')
+                {
+                    strArray[lastIdx] = '0';
+                    strArray[lastIdx - 1] = '2';
+                    strArray[lastIdx - 2] = '%';
+                    lastIdx = lastIdx - 3;
+                }
+                else
+                {
+                    strArray[lastIdx--] = str[i];
+                }
+            }
+
+            return new String(strArray);
+        }   
+    }
+}
