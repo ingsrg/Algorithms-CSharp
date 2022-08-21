ae4c11c28c57913e5c75bc87750a882d662418f3
Sergio Ramirez <ingsrg@gmail.com>
2022-08-21 14:44:01 -0500

    PalindromePerm

---
+++ PalindromePerm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PalindromePerm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PalindromePerm("acaa"));
+        }
+
+        static bool PalindromePerm(string word)
+        {
+            int[] mem = new int[26];
+            int countOdd = 0;
+
+            for(int i=0; i < word.Length; i++)
+            {
+                if(word[i] >= 'a' && word[i] <= 'z')
+                {
+                    int val = word[i] - 'a';
+
+                    mem[val]++;
+                    if(mem[val] % 2 == 0)
+                        countOdd --;
+                    else
+                        countOdd ++;
+                }
+            }
+
+            return countOdd < 2;
+        }
+    }
+}
