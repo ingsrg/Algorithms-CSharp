ba919865c7532c3cf45d6fb987f4c193af2284ee
Sergio Ramirez <ingsrg@gmail.com>
2022-09-10 11:30:12 -0500

    SingleNumber

---
+++ SingleNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingleNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SingleNumber(new int[] {1, 2, 3, 2}));
+        }
+
+        static int SingleNumber(int[] nums) 
+        {
+            int r = 0;
+            foreach(int item in nums)
+            {
+                r ^= item;
+            }
+            
+            return r;
+        }
+    
+        static int SingleNumber1(int[] nums) {
+            HashSet<int> hs = new HashSet<int>();
+            int r = 0;
+            
+            foreach(int item in nums)
+            {
+                if(hs.Contains(item))
+                    hs.Remove(item);
+                else
+                    hs.Add(item);
+            }
+            
+            foreach(int item in hs)
+                r = item;
+            
+            return r;
+        }
+
+
+    }
+}
