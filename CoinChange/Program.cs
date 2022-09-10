bc7390a8c2820309770770029b478aee12703aa8
Sergio Ramirez <ingsrg@gmail.com>
2022-09-10 10:29:59 -0500

    RotateArray

---
+++ CoinChange/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CoinChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 3;
+            int[] M = new int[] {2, 4};
+
+            Console.WriteLine(CoinChange(M, k));
+        }
+
+        static int CoinChange(int[] M, int k)
+        {
+            int[] dp = new int[k+1];
+
+            for(int i=1; i <= k; i++)
+            {
+                dp[i] = k + 1;
+            }
+
+            for(int i=1; i <= k; i++)
+            {
+                for(int j=0; j < M.Length; j++)
+                {
+                    if(i - M[j] >= 0)
+                    {
+                        dp[i] = Math.Min(dp[i], 1 + dp[i - M[j]]);
+                    }
+                   // else
+                   // {
+                    //    dp[i] = -1;
+                   // }
+                }
+            }
+            return dp[k] > k ? -1 : dp[k];
+        }
+    }
+}
---
+++ RotateArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RotateArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] {1, 2, 3, 4, 5, 6, 7};
+            RotateArray(nums, 3);
+            for(int i=0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+
+        static void RotateArray(int[] nums, int k)
+        {
+            k = k % nums.Length;
+
+            Reverse(nums, 0, nums.Length - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, nums.Length - 1);
+        }
+
+        static void Reverse(int[] nums, int start, int end)
+        {
+            while(start < end)
+            {
+                int tmp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = tmp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
