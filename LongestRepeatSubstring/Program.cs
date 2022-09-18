1130953192b17e6ad5a41c10f2cd2325de9e32b6
Sergio Ramirez <ingsrg@gmail.com>
2022-09-18 12:11:18 -0500

    LongestRepeatedSubstring

---
+++ LongestRepeatSubstring/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestRepeatSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestRepeatSubstring("banana"));
+        }
+
+        static private string LongestRepeatSubstring(string s)
+        {
+            HashSet<string> hs = new HashSet<string>();
+
+            string result = "";
+
+            for(int i=0; i < s.Length; i++)
+            {
+                for(int j=1; j <= s.Length - i; j++)
+                {
+                    string tmp = s.Substring(i, j);
+                    if(hs.Contains(tmp))
+                    {
+                        if(tmp.Length > result.Length)
+                            result = tmp;
+                    }
+                    else
+                        hs.Add(tmp);
+                }
+            }
+            return result;
+        }
+    }
+
+
+}
