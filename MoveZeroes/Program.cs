718cc16718288caa71c2223c6971ac00c5964db1
Sergio Ramirez <ingsrg@gmail.com>
2022-10-30 11:43:17 -0600

    MoveZeroes

---
+++ MoveZeroes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MoveZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tmp = new int[] {0,1,0,3,12};
+            MoveZeroes(tmp);
+            foreach(int item in tmp)
+                Console.WriteLine(item);
+        }
+
+        static void MoveZeroes(int[] nums) {
+            int idx = 0;
+            
+            if(nums.Length == 1)
+                return;
+            
+            for(int i=0; i < nums.Length; i++)
+            {
+                if(nums[i] != 0)
+                {
+                    nums[idx] = nums[i];
+                    if(idx != i) 
+                        nums[i] = 0;
+                    idx++;
+                }
+                
+            }
+        }
+    }
+}
