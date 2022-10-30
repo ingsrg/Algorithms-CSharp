e764515a826075ad5b1c0bc1b1f02391b0aa1ff3
Sergio Ramirez <ingsrg@gmail.com>
2022-10-30 13:29:24 -0600

    FirstBadVersion

---
+++ FirstBadVersion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FirstBadVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int FirstBadVersion(int n) {
+            int left = 1;
+            int right = n;
+            
+            while(left < right)
+            {
+                int mid = left + (right-left)/2;
+                
+                if(!IsBadVersion(mid))
+                    left = mid + 1;
+                else
+                    right = mid;
+            }
+            
+            return left;
+        }
+    }
+}
